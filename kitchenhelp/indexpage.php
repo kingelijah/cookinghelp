d1eaa2237363542f1fdbd92f74363d5165a59a23
kingelijah <ealeakhue@gmail.com>
2017-05-10 07:26:33 +0100

    add files

---
+++ kitchenhelp/indexpage.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pirata One">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Bilbo">
+	<link rel="stylesheet" href="mdl/material.min.css">
+    <script src="mdl/material.min.js"></script>
+   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+   <link rel="stylesheet" href="font/css/font-awesome.min.css">
+   <link rel="stylesheet" href="mystyles.css">
+   <title>Cooking Help </title>
+   <style>
+.demo-card-square.mdl-card {
+  width: 360px;
+  height: 360px;
+}
+.demo-card-square > .mdl-card__title1 {
+  color: #fff;
+  background:
+    url('images/first.jpg') bottom right 45% no-repeat #46B6AC;
+}
+.demo-card-square > .mdl-card__title2 {
+  color: #fff;
+  background:
+    url('images/eant.PNG') bottom right 45% no-repeat #46B6AC;
+	
+}
+.demo-card-square > .mdl-card__title3 {
+  color: #fff;
+  background:
+    url('images/eantim.jpg') bottom right 45% no-repeat #46B6AC;
+}
+page-header{ 
+  background:
+  url('images/eantim.jpg')
+ }
+   </style>
+</head>
+<body>
+<br/>
+<nav id="mynavbar" class="navbar-inverse navbar-fixed-top" role="navigation" style="font-family:'Pirata One',serif; font-size:'40';">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button"class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav" >
+                                <li><div class="dropdown"><a href="home.php">Home</a></div></li>
+                                <li><div class="dropdown"><a href="aboutus.php">About Us</a></div></li>
+								<li><div class="dropdown"><a href="contactus.php">Contact Us</a></div></li>
+                       <li><div class="dropdown">
+                    <a href="ourservices.php" class="dropbtn">Our Services<b class="caret"></b></a>
+                    <div class="dropdown-content">
+                        <a href="ourservices.php#eh">Event Help</a>
+                        <a href="ourservices.php#hh">Home Help</a>
+                        <a href="ourservices.php#th">Training Help</a>
+                    </div></div></li>
+								<li><div class="dropdown"><a href="bookaservice.php">Book A Service</a></div></li>
+								<li><div class="dropdown">
+                        <a href="#" class="dropbtn">Exclusive Tips<b class="caret"></b></a>
+                        <div class="dropdown-content">
+                        <a href="cookingtips.php">Cooking Tips</a>
+                        <a href="kitchentips.php">Kitchen Tips</a>
+                        </div></div></li>
+                                    </ul>
+                                </li>
+								</ul>
+                        </div>
+						</div>
+			            </div>
+</nav>
+</br>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+		</body>
+		</html>
+	
