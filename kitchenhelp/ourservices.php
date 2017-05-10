a8ac5f9c3c8f8a14725747284e91a8d3222d4f15
kingelijah <ealeakhue@gmail.com>
2017-05-10 07:31:28 +0100

    add files

---
+++ kitchenhelp/ourservices.php
@@ -0,0 +1,30 @@
+<?php
+require("indexpage.php");
+?>
+<section class="ourservices-section">
+<div class="container">
+<div class="col-md-10 col-md-offset-1">
+<div id="eh">
+<h2 style="text-align:center;font-family:'Pirata One',serif;">Event Help</h2>
+<h3 style="font-family:'Bilbo',serif;">we handle all round cooking and food services for your various events.</br>Our duty is to make sure your guests get the best treatment in terms of food quality
+and other events services to ensure your satisfaction.</br>We understand the fact that having a successful event means having quality food and quality services also.
+</h3>
+
+</div>
+
+<div id="hh">
+<h2 style="text-align:center; font-family:'Pirata One',serif;">Home Help</h2>
+<h3 style="font-family:'Bilbo',serif;">we provide daily home cooking to our clients because we understand how tedious and painful cooking can be especially when you dont have the right hands 
+for the job.</br> we provide first of its kind services to take care of daily home cooking.</br>All you need is to contact us and we will surely do the rest for you,
+from well tailored meal timetable to food preparation and services.</h3>
+
+</div>
+<div id="th">
+<h2 style="text-align:center; font-family:'Pirata One',serif;">Training Help</h2>
+<h3 style="font-family:'Bilbo',serif;">we give standard professional trainings to prospective chefs,we are known for practical one-on-one training on kitchen etiquettes and quality food preparation 
+we have well tailored courses that suits you.</br>Our trainings our 100% practical and we also award certificates on completion.</h3>
+
+</div>
+</div>
+</div>
+</section>
