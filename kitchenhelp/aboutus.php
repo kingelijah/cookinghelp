77a9f35c3b6902ab351e117981d50df8bb4a0a2b
kingelijah <ealeakhue@gmail.com>
2017-05-10 07:21:10 +0100

    add files

---
+++ kitchenhelp/aboutus.php
@@ -0,0 +1,16 @@
+	
+<?php
+ require("indexpage.php");
+ ?> 
+ </br>
+ <section class="aboutus-section">
+ <div class="container containers">
+ <div class="col-md-10 col-md-offset-1">
+ <h2 style="text-align:center;font-family:'Pirata One',serif;">we are helpers in your kitchen </h2>
+ <h3 style="font-family:'Bilbo',serif;">with almost 30 years industry experience, taking care of your daunting cooking needs is never a problem. over the years we have come to realize how tedious 
+ and painful cooking can be especially when you dont have the right hands for the job.</br>Be it event cooking,home cooking, and training of personnels.so we have 
+ decide to take it upon ourselves to handle that painful task of yours and make sure we put an unending smile to your face by giving you 100% satisfaction,
+achieving that is very possible because when it comes to cooking and other kitchen duties we are perfect.</br>Talk to us today and we will do the rest.</h3> 
+ </div>
+ </div>
+ </section>
