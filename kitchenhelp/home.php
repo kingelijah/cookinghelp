1e3b0667eb36ec16c8d786b234e652f7882a8dc0
kingelijah <ealeakhue@gmail.com>
2017-05-10 07:25:04 +0100

    ad files

---
+++ kitchenhelp/home.php
@@ -0,0 +1,121 @@
+<?php
+  require("indexpage.php")
+  ?>
+
+
+<div class="row">
+<div id="mycarousel" class="carousel slide carousel-fade" data-ride="carousel"  data-interval="4000">
+<ol class="carousel-indicators">
+<li data-target="#mycarousel" data-slide-to="0" class="active"></li>
+<li data-target="#mycarousel" data-slide-to="1"></li>
+<li data-target="#mycarousel" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner" role="listbox">
+<div class="item active">
+<img src="images/noma.jpg" class="img-responsive"/>
+<div class="carousel-caption">
+<h3 style="font-family:'Pirata One',serif; color:white; font-size:50px;">we cover a wide range of traditional and local dishes that cuts across major tribes in the counrty.</h3>
+<a href="bookaservice.php"><button type="button" class="btn btn-success"><b>Book A service</b></button></a>
+</div>
+</div>
+<div class="item">
+<img src="images/no.jpg" class="img-responsive" />
+<div class="carousel-caption">
+<h3 style="font-family:'Pirata One',serif; color:white; font-size:50px;">Be it continental meals your satisfaction is fully guaranteed</h3>
+<a href="bookaservice.php"><button type="button" class="btn btn-success"><b>Book A service</b></button></a>
+</div>
+</div>
+<div class="item">
+<img src="images/noms.jpg" class="img-responsive"/>
+<div class="carousel-caption">
+<h3 style="font-family:'Pirata One',serif; color:white; font-size:50px;">With almost 30 years industry experience we are fully confident to meet your needs.</h3>
+<a href="bookaservice.php"><button type="button" class="btn btn-success"><b>Book A service</b></button></a>
+</div>
+</div>
+<div class="left carousel-control" href="#mycarousel" data-slide="prev">
+<span class="sr-only">previous</span></div>
+<div class="right carousel-control" href="#mycarousel" data-slide="next">
+<span class="sr-only">next</span></div>
+</div>
+</div>
+</div>
+</div>
+<br/>
+<div class="container">
+			
+    <div class="row">
+	<div class="col-md-4 col-sm-4">
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title1 mdl-card--expand">
+    </div>
+  <div class="mdl-card__supporting-text">
+  <h2 class="mdl-card__title-text" style="font-family:'Pirata One',serif; color:black;"><b>Event help</b></h2>
+   <h4 style="font-family:'Bilbo',serif;"><b> we handle all round cooking and food services for your various events...</b></h4>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" style="color:green;">
+      <a href="ourservices.php#eh"><b>Read more</b></a>
+    </a>
+  </div>
+</div>
+	</div>
+	<div class="col-md-4 col-sm-4">
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title2 mdl-card--expand">
+    
+  </div>
+  <div class="mdl-card__supporting-text">
+  <h2 class="mdl-card__title-text" style="font-family:'Pirata One',serif;color:black;"><b>Home help</b></h2>
+    <h4 style="font-family:'Bilbo',serif;"><b>we provide daily home cooking to our clients because we...</b></h4>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"style="color:green;">
+      <a href="ourservices.php#eh"><b>Read more</b></a>
+    </a>
+  </div>
+</div>
+</div>
+	<div class="col-md-4 col-sm-4">
+<div class="demo-card-square mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title3 mdl-card--expand">
+    
+  </div>
+  <div class="mdl-card__supporting-text">
+  <h2 class="mdl-card__title-text" style="font-family:'Pirata One',serif;color:black;"><b>Training help</b></h2>
+   <h4 style="font-family:'Bilbo',serif;"><b> we give standard professional trainings to prospective chefs,we...</b></h4>
+  </div>
+  <div class="mdl-card__actions mdl-card--border">
+    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"style="color:green;">
+     <a href="ourservices.php#eh"><b>Read more</b></a>
+    </a>
+  </div>
+</div>
+	</div>
+	</div>
+	</div>
+<br/><br/>
+<div class="container">
+<hr style="border: 1px; color:green; box-shadow: 0 0 10px 1px green;"></hr>
+</div>
+<div class="container">
+<div class="row" style="font-family:'Pirata One',serif;" >
+<div class="col-md-4 col-sm-4">
+<div class="ribbon2"><img src="images/vb.jpg" height="100" width="80"></div>
+<p style="font-size:20px;">our chefs are industry professionals,friendly,neat and trusted.</p>
+</div>
+<div class="col-md-4 col-sm-4">
+<div class="ribbon3"><i class="fa fa-check-square-o fa-5x"></i></div>
+<p style="font-size:20px;">our services are money back guaranteed</p>
+</div>
+<div class="col-md-4 col-sm-4">
+<div class="ribbon4"><i class="fa fa-pencil-square-o fa-5x"></i></div>
+<div><p style="font-size:20px;">you book a service and we do the rest.</p></div>
+</div>
+</div>
+</div>
+<br/><br/>
+<footer>
+<div class="container">
+<p >web powered by emperor technologies</br>ealeakhue@gmail.com</p>
+</div>
+</footer>
